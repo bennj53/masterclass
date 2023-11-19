a76fb2cdbf96a0ddc74e0284475da50288939d15
Benoît JULLIOT <b.julliot@octo.com>
2023-11-19 19:38:19 +0100

    test du RestaurantService

--- src/test/java/com/octo/masterclass/restaurant/RestaurantServiceTest.java
+++ src/test/java/com/octo/masterclass/restaurant/RestaurantServiceTest.java
@@ -1,13 +1,30 @@
 package com.octo.masterclass.restaurant;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
 class RestaurantServiceTest {
 
     @Test
     void calculerPrixFormule() {
-        assertTrue(true);
+        //Given
+        RestaurantService restaurantService = new RestaurantService();
+        Formule formule = new Formule("formule test", List.of(
+                new Plat("ceviche", 20.50, List.of(
+                        new Ingredient("citron", false, 0),
+                        new Ingredient("poisson", true, 0)
+                )),
+                new Plat("tiramisu", 9.50, List.of(
+                        new Ingredient("café", false, 0),
+                        new Ingredient("mascarpone", true, 0)
+                ))
+                )
+        );
+        //When
+        double prix = restaurantService.calculerPrixFormule(formule);
+        //Then
+        Assertions.assertEquals(30.0, prix);
     }
 }
